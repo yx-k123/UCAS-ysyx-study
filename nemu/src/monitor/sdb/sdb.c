864cf4f3b90c10c544b33117b7e208e6a51fcbc2
tracer-ysyx <tracer@ysyx.org>
2026-03-17 14:11:14 +0800

    > compile NEMU 2023K8009922004 kouyixin Linux kouyixin-virtual-machine 6.8.0-101-generic #101~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 11 13:19:54 UTC  x86_64 x86_64 x86_64 GNU/Linux  14:11:14 up 37 min,  1 user,  load average: 1.56, 0.58, 0.36

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -52,6 +52,15 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args) {
+  int n = 1;
+  if (args != NULL) {
+    sscanf(args, "%d", &n);
+  }
+  cpu_exec(n);
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -62,9 +71,8 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
-  /* TODO: Add more commands */
-
+  { "si", "Step into instruction, default steps = 1", cmd_si },
+  
 };
 
 #define NR_CMD ARRLEN(cmd_table)
