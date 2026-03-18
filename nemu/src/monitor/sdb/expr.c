6423825d27a79a00d937ba08a336ca066de8a97d
tracer-ysyx <tracer@ysyx.org>
2026-03-18 16:21:11 +0800

    > compile NEMU 2023K8009922004 kouyixin Linux kouyixin-virtual-machine 6.8.0-101-generic #101~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 11 13:19:54 UTC  x86_64 x86_64 x86_64 GNU/Linux  16:21:11 up  1:11,  1 user,  load average: 0.97, 0.60, 0.45

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -205,8 +205,6 @@ int find_main_operator(int p, int q) {
   return op;
 }
 
-// extern word_t isa_reg_str2val(const char *s, bool *success);
-
 word_t eval(int p, int q) {
   if (p > q) {
     assert(0);
