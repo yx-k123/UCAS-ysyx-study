07e51a59215cf3c4bacc86f6fc801097fe83a565
kouyixin ubuntu22.04 <1713284633@qq.com>
2026-05-17 18:49:15 +0800

    ftrace

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -36,7 +36,7 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host(addr), len, data);
 }
 
-static void out_of_bound(paddr_t addr) {
+static void __attribute__((unused)) out_of_bound(paddr_t addr) {
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
 }
