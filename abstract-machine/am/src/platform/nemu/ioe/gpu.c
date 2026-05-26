7117c067f7c253e9bdd365e26add935b251e17e6
kouyixin ubuntu22.04 <1713284633@qq.com>
2026-05-26 17:17:54 +0800

    VGA

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -7,14 +7,43 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t info = inl(VGACTL_ADDR);
+  int w = info >> 16;
+  int h = info & 0xffff;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = w, .height = h,
+    .vmemsz = w * h * sizeof(uint32_t)
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t info = inl(VGACTL_ADDR);
+  int screen_w = info >> 16;
+  int screen_h = info & 0xffff;
+  int x = ctl->x;
+  int y = ctl->y;
+  int w = ctl->w;
+  int h = ctl->h;
+
+  if (ctl->pixels != NULL && w > 0 && h > 0 && x < screen_w && y < screen_h) {
+    int copy_w = w;
+    int copy_h = h;
+    if (x + copy_w > screen_w) copy_w = screen_w - x;
+    if (y + copy_h > screen_h) copy_h = screen_h - y;
+
+    if (copy_w > 0 && copy_h > 0) {
+      uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+      uint32_t *src = (uint32_t *)ctl->pixels;
+      for (int j = 0; j < copy_h; j ++) {
+        uint32_t *dst = fb + (y + j) * screen_w + x;
+        uint32_t *row = src + j * w;
+        for (int i = 0; i < copy_w; i ++) {
+          dst[i] = row[i];
+        }
+      }
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
