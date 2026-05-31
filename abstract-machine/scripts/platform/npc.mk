abb4136b33036c5b1a9913358c8dcde0f5454f04
kouyixin ubuntu22.04 <1713284633@qq.com>
2026-05-31 20:19:14 +0800

    add npc difftest

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -12,6 +12,7 @@ NPC_HOME ?= $(abspath $(AM_HOME)/../npc)
 NEMU_HOME ?= $(abspath $(AM_HOME)/../nemu)
 INC_CAPSTONE := -I$(NEMU_HOME)/tools/capstone/repo/include
 LIB_CAPSTONE := $(NEMU_HOME)/tools/capstone/repo/libcapstone.so.5
+DIFF_SO ?= $(NEMU_HOME)/build/riscv32-nemu-interpreter-so
 
 insert-arg: image
 	@python3 $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
@@ -21,10 +22,17 @@ image: image-dep
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
+# run: insert-arg
+# 	@if [ ! -d "$(NPC_HOME)" ]; then echo "NPC_HOME is invalid: $(NPC_HOME)"; exit 1; fi
+# 	@if ! command -v verilator >/dev/null 2>&1; then echo "verilator not found"; exit 1; fi
+# 	@verilator --trace --cc --exe --build --top-module top -CFLAGS "-O0 $(INC_CAPSTONE)" -LDFLAGS "$(LIB_CAPSTONE) -ldl -Wl,-rpath,$(NEMU_HOME)/tools/capstone/repo" -Mdir "$(NPC_HOME)/build/obj_dir" "$(NPC_HOME)/csrc/main.cpp" "$(NPC_HOME)"/vsrc/*.v
+# 	@"$(NPC_HOME)/build/obj_dir/Vtop" "$(IMAGE).bin" "$(IMAGE).elf"
+
 run: insert-arg
+	@if [ ! -f "$(DIFF_SO)" ]; then echo "DIFF_SO not found: $(DIFF_SO)"; exit 1; fi
 	@if [ ! -d "$(NPC_HOME)" ]; then echo "NPC_HOME is invalid: $(NPC_HOME)"; exit 1; fi
 	@if ! command -v verilator >/dev/null 2>&1; then echo "verilator not found"; exit 1; fi
-	@verilator --trace --cc --exe --build --top-module top -CFLAGS "-O0 $(INC_CAPSTONE)" -LDFLAGS "$(LIB_CAPSTONE) -Wl,-rpath,$(NEMU_HOME)/tools/capstone/repo" -Mdir "$(NPC_HOME)/build/obj_dir" "$(NPC_HOME)/csrc/main.cpp" "$(NPC_HOME)"/vsrc/*.v
-	@"$(NPC_HOME)/build/obj_dir/Vtop" "$(IMAGE).bin" "$(IMAGE).elf"
+	@verilator --trace --cc --exe --build --top-module top -CFLAGS "-O0 $(INC_CAPSTONE)" -LDFLAGS "$(LIB_CAPSTONE) -ldl -Wl,-rpath,$(NEMU_HOME)/tools/capstone/repo" -Mdir "$(NPC_HOME)/build/obj_dir" "$(NPC_HOME)/csrc/main.cpp" "$(NPC_HOME)"/vsrc/*.v
+	@"$(NPC_HOME)/build/obj_dir/Vtop" "$(IMAGE).bin" "$(IMAGE).elf" --diff="$(DIFF_SO)"
 
-.PHONY: insert-arg
+.PHONY: insert-arg run
