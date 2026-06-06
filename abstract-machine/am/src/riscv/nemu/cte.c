3f1af4edb790bc28a143f4de0da83f262e3608c4
tracer-ysyx <tracer@ysyx.org>
2026-06-06 21:05:22 +0800

    > compile NEMU 2023K8009922004 kouyixin Linux kouyixin-virtual-machine 6.8.0-117-generic #117~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu May  7 22:17:46 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:05:22 up 7 min,  1 user,  load average: 0.30, 0.19, 0.10

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -26,6 +26,9 @@ extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
+  void* addr = &__am_asm_trap;
+  printf("Address of __am_asm_trap: %p\n", addr);
+  
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
   // register event handler
