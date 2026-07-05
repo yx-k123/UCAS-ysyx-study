fbaf7147c2ce06f105f97e1974aa326fc092e2b3
tracer-ysyx <tracer@ysyx.org>
2026-07-05 17:21:15 +0800

    > sim RTL 2023K8009922004 kouyixin Linux kouyixin-virtual-machine 6.8.0-117-generic #117~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu May  7 22:17:46 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:21:15 up  1:54,  1 user,  load average: 0.73, 0.80, 0.79

---
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -0,0 +1,35 @@
+AM_SRCS := riscv/ysyxsoc/start.S         riscv/ysyxsoc/trm.c          riscv/npc/ioe.c               riscv/npc/timer.c             riscv/npc/input.c             riscv/npc/cte.c               riscv/npc/trap.S              platform/dummy/vme.c          platform/dummy/mpe.c
+
+CFLAGS    += -fdata-sections -ffunction-sections
+LDSCRIPTS += $(AM_HOME)/scripts/linker-ysyxsoc.ld
+LDFLAGS   += --defsym=_mrom_start=0x20000000 --defsym=_mrom_size=0x1000
+LDFLAGS   += --defsym=_sram_start=0x0f000000 --defsym=_sram_size=0x2000
+LDFLAGS   += --defsym=_stack_size=0x1000
+LDFLAGS   += --gc-sections -e _start
+
+MAINARGS_MAX_LEN = 64
+MAINARGS_PLACEHOLDER = the_insert-arg_rule_in_Makefile_will_insert_mainargs_here
+CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=$(MAINARGS_PLACEHOLDER)
+
+NPC_HOME ?= $(abspath $(AM_HOME)/../npc)
+TRACE ?= 0
+WAVE ?= 0
+MAX_CYCLES ?= 200000
+NPC_ARGS ?=
+
+RUN_ARGS := --trace=$(TRACE) --wave=$(WAVE) --max-cycles=$(MAX_CYCLES) $(NPC_ARGS)
+
+insert-arg: image
+	@python3 $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
+
+image: image-dep
+	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
+run: insert-arg
+	@if [ ! -d "$(NPC_HOME)" ]; then echo "NPC_HOME is invalid: $(NPC_HOME)"; exit 1; fi
+	@$(MAKE) -s -C "$(NPC_HOME)" sim
+	@"$(NPC_HOME)/build/obj_dir/npc-sim" "$(IMAGE).bin" "$(IMAGE).elf" $(RUN_ARGS)
+
+.PHONY: insert-arg run
--- npc/tests/char-test.c
+++ npc/tests/char-test.c
@@ -3,7 +3,6 @@
 
 void _start(void) {
   *(volatile char *)(UART_BASE + UART_TX) = 'A';
-  *(volatile char *)(UART_BASE + UART_TX) = '\n';
   while (1) {
   }
 }
