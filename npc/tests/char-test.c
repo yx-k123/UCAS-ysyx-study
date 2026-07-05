386e2318d1f1fd53c8d03eb81b8f4ae0b468e0f9
tracer-ysyx <tracer@ysyx.org>
2026-07-05 17:12:53 +0800

    > sim RTL 2023K8009922004 kouyixin Linux kouyixin-virtual-machine 6.8.0-117-generic #117~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu May  7 22:17:46 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:12:53 up  1:46,  1 user,  load average: 1.05, 1.16, 0.91

--- npc/tests/char-test.c
+++ npc/tests/char-test.c
@@ -3,7 +3,7 @@
 
 void _start(void) {
   *(volatile char *)(UART_BASE + UART_TX) = 'A';
-  // *(volatile char *)(UART_BASE + UART_TX) = '\n';
+  *(volatile char *)(UART_BASE + UART_TX) = '\n';
   while (1) {
   }
 }
